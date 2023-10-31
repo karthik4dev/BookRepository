2ee2fa83e2c92512a738501bc90ce78c7f7d697d
Karthik P N <Karthikpnrao.97@gmail.com>
2023-10-31 15:09:08 +0530

    adding build folder

--- src/main/java/com/KarthikProject/BooksRepository/Repositories/BookRepositoryRepo.java
+++ src/main/java/com/KarthikProject/BooksRepository/Repositories/BookRepositoryRepo.java
@@ -2,9 +2,14 @@ package com.KarthikProject.BooksRepository.Repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.KarthikProject.BooksRepository.Entities.Book;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
 import java.util.List;
 
 
@@ -18,5 +23,5 @@ public interface BookRepositoryRepo extends CrudRepository<Book, Integer> {
 	
 	public Book findByBookNameString(String bookNameString);
 	public List<Book> findByIsbn(String isbn);
-
+	
 }
