862b42abbfd389fddf409a4e863d1d1a9378bba2
karthik4dev <70136176+karthik4dev@users.noreply.github.com>
2024-05-01 23:57:55 +0530

    Squashed commit of the following:
    commit d6256e59b860ce34fd25af8f958cd809f242c1a8
    Author: Karthik P N <Karthikpnrao.97@gmail.com>
    Date:   Tue Apr 9 19:51:56 2024 +0530
    
        reverting back

--- src/main/java/com/KarthikProject/BooksRepository/DTOs/BookAttributesDTO.java
+++ src/main/java/com/KarthikProject/BooksRepository/DTOs/BookAttributesDTO.java
@@ -1,12 +1,14 @@
 package com.KarthikProject.BooksRepository.DTOs;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class BookAttributesDTO {
     private long bookID;
     private int bookPages;
--- src/main/java/com/KarthikProject/BooksRepository/DTOs/BookRepositoryDTO.java
+++ src/main/java/com/KarthikProject/BooksRepository/DTOs/BookRepositoryDTO.java
@@ -2,12 +2,14 @@ package com.KarthikProject.BooksRepository.DTOs;
 
 import com.KarthikProject.BooksRepository.Entities.Author;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class BookRepositoryDTO {
     private int id;
     private String bookNameString;
