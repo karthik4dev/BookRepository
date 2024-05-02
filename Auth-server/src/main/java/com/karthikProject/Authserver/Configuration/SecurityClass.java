c8f9452c2cba09cb356d4a13e0ad80b502aea1e8
karthik4dev <70136176+karthik4dev@users.noreply.github.com>
2024-05-02 23:19:34 +0530

    Added HttpBasic

--- Auth-server/src/main/java/com/karthikProject/Authserver/Configuration/SecurityClass.java
+++ Auth-server/src/main/java/com/karthikProject/Authserver/Configuration/SecurityClass.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,8 +23,10 @@ public class SecurityClass {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/users/**").permitAll().anyRequest().authenticated())
-                .csrf((csrf) -> csrf.disable()).build();
+        return http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/users/**").permitAll()
+                        .anyRequest().authenticated())
+                .csrf((csrf) -> csrf.disable()).httpBasic(Customizer.withDefaults()).build();
     }
 
     @Bean
